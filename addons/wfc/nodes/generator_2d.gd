a69c6e7541b5effc6283925cd988f304f0d28a14
Flavien Normand <contact@flavien-normand.fr>
2023-11-10 15:29:41 +0100

    Add layered tilemap mapper (#4)
    ---------
    
    Co-authored-by: Alexey Bondarenko <alexey.bond.94.55@gmail.com>

--- addons/wfc/nodes/generator_2d.gd
+++ addons/wfc/nodes/generator_2d.gd
@@ -70,7 +70,7 @@ func _create_runner() -> WFCSolverRunner:
 func _create_mapper(map: Node) -> WFCMapper2D:
 	match map.get_class():
 		"TileMap":
-			return WFCTileMapMapper2D.new()
+			return WFCLayeredTileMapMapper2D.new()
 		"GridMap":
 			return WFCGridMapMapper2D.new()
 		var cname:
@@ -110,10 +110,8 @@ func start():
 		
 		if rules.mapper == null:
 			rules.mapper = _create_mapper(target_node)
-
 		if not rules.mapper.is_ready():
 			rules.mapper.learn_from(positive_sample_node)
-		
 		rules.learn_from(positive_sample_node)
 		
 		if rules.complete_matrices and negative_sample != null and not negative_sample.is_empty():
@@ -159,10 +157,3 @@ func _ready():
 func _process(_delta):
 	if _runner != null and _runner.is_running():
 		_runner.update()
-
-
-
-
-
-
-
